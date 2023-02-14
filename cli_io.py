b927b18f266e8bcc9872d35d3ab65aca51c18741
Eboubaker Bekkouche <eboubakkar@gmail.com>
2023-02-14 17:24:30 +0000

    read text from user

---
+++ cli_io.py
@@ -0,0 +1,316 @@
+import sys
+import threading
+import traceback
+from typing import List, Optional
+
+from readchar import readkey, key
+from termcolor import colored
+
+
+class ReentrantRWLock:
+    """
+    A lock object that allows many simultaneous "read locks", but only one "write lock."
+    it also ignores multiple write locks from the same thread
+    """
+
+    def __init__(self):
+        super().__init__()
+        self._writer = None  # current writer
+        self._readers: List[int] = []  # list of unique readers
+        self._read_ready = threading.Condition(threading.Lock())
+        self._with_ops_write = []  # stack for 'with' keyword for write or read operations, 0 for read 1 for write
+        self._ops_arr_lock = threading.Lock()  # lock for previous list
+
+    def acquire_read(self):
+        """
+        Acquire a read lock. Blocks only if a another thread has acquired the write lock.
+        """
+        ident = threading.current_thread().ident
+        if self._writer == ident or ident in self._readers:
+            return
+        with self._read_ready:
+            self._readers.append(ident)
+
+    def release_read(self):
+        """
+        Release a read lock if exists from this thread
+        """
+        ident = threading.current_thread().ident
+        if self._writer == ident or ident not in self._readers:
+            return
+        with self._read_ready:
+            self._readers.remove(ident)
+            if len(self._readers) == 0:
+                self._read_ready.notifyAll()
+
+    def acquire_write(self):
+        """
+        Acquire a write lock. Blocks until there are no acquired read or write locks from another thread.
+        """
+        ident = threading.current_thread().ident
+        if self._writer == ident:
+            return
+        self._read_ready.acquire()
+        me_included = 1 if ident in self._readers else 0
+        while len(self._readers) - me_included > 0:
+            self._read_ready.wait()
+        self._writer = ident
+
+    def release_write(self):
+        """
+        Release a write lock if exists from this thread.
+        """
+        if not self._writer or not self._writer == threading.current_thread().ident:
+            return
+        self._writer = None
+        self._read_ready.release()
+
+    def __enter__(self):
+        with self._ops_arr_lock:
+            if len(self._with_ops_write) == 0:
+                raise RuntimeError("ReentrantRWLock: used 'with' block without call to for_read or for_write")
+            write = self._with_ops_write[-1]
+        if write:
+            self.acquire_write()
+        else:
+            self.acquire_read()
+
+    def __exit__(self, exc_type, exc_value, tb):
+        with self._ops_arr_lock:
+            write = self._with_ops_write.pop()
+        if write:
+            self.release_write()
+        else:
+            self.release_read()
+        if exc_type is not None:
+            return False  # exception happened
+        return True
+
+    def for_read(self):
+        """
+        used for 'with' block
+        """
+        with self._ops_arr_lock:
+            self._with_ops_write.append(0)
+        return self
+
+    def for_write(self):
+        """
+        used for 'with' block
+        """
+        with self._ops_arr_lock:
+            self._with_ops_write.append(1)
+        return self
+
+class ReadError(IOError):
+    pass
+
+
+class IO:
+    def __init__(self):
+        self.read_buffer = ''
+        self.label = ''
+        self.label_colored = ''
+        self.read_lock = threading.Lock()
+        self.write_lock = threading.RLock()
+        self.buffer_lock = ReentrantRWLock()
+        self.last_line = ''
+        self.last_writer_was_reader = False
+        self.read_interrupted_buffer = ''
+        self.label_color = 'white'
+        self.history: List[str] = []
+        self.history_tail_index = 0
+        self.read_error: Optional[ReadError] = None
+        self.read_interrupted = False
+        self.cursor_at = 0
+
+    def update_input_label(self, label):
+        self.label = label
+        self.label_colored = colored(self.label, self.label_color)
+        if self.read_lock.locked():
+            self.__write_input()
+
+    def __clear_input(self):
+        with self.write_lock:
+            sys.stdout.write('\r')
+            sys.stdout.write(''.join(' ' for _ in range(len(self.last_line))))
+            sys.stdout.write('\r')
+            self.last_line = ''
+            sys.stdout.flush()
+
+    def __move_cursor(self, txt: str, at: int):
+        with self.write_lock:
+            sys.stdout.write('\r')
+            sys.stdout.write(txt[:at])
+            sys.stdout.flush()
+
+    def __write_input(self, append=''):
+        with self.write_lock:
+            self.__clear_input()
+            with self.buffer_lock.for_read():
+                self.last_line = self.label_colored + self.read_buffer + append
+                sys.stdout.write(self.last_line)
+                self.__move_cursor(self.last_line, self.cursor_at + len(self.label_colored))
+                self.last_writer_was_reader = True
+
+    def update_input_label_color(self, color):
+        """
+        Available text colors:
+            grey, red, green, yellow, blue, magenta, cyan, white.
+        """
+        self.label_color = color
+        self.label_colored = colored(self.label, self.label_color)
+        if self.read_lock.locked():
+            self.__write_input()
+
+    def __command_left_key(self):
+        with self.write_lock:
+            if self.cursor_at > 0:
+                self.cursor_at -= 1
+                self.__move_cursor(self.last_line, self.cursor_at + len(self.label_colored))
+
+    def __command_right_key(self):
+        with self.write_lock:
+            if self.cursor_at < len(self.last_line) - len(self.label_colored):
+                self.cursor_at += 1
+                self.__move_cursor(self.last_line, self.cursor_at + len(self.label_colored))
+
+    def __command_up_key(self):
+        with self.write_lock:
+            if self.history_tail_index > 0:
+                self.history_tail_index -= 1
+                self.update_input_buffer(self.history[self.history_tail_index])
+
+    def __command_down_key(self):
+        with self.write_lock:
+            if len(self.history) - 1 > self.history_tail_index:
+                self.history_tail_index += 1
+                self.update_input_buffer(self.history[self.history_tail_index])
+
+    def __command_delete_key(self):
+        with self.write_lock:
+            if self.cursor_at < len(self.read_buffer):
+                # delete char after cursor
+                self.read_buffer = self.read_buffer[:self.cursor_at] + self.read_buffer[self.cursor_at + 1:]
+                self.__write_input()
+
+    def handle_stroke(self, char):
+        with self.buffer_lock.for_read():
+            if char == key.UP:  # up
+                self.__command_up_key()
+            elif char == key.DOWN:  # bottom
+                self.__command_down_key()
+            elif char == key.LEFT:  # left
+                self.__command_left_key()
+            elif char == key.RIGHT:  # right
+                self.__command_right_key()
+            elif char == key.DELETE:  # delete
+                self.__command_delete_key()
+            else:
+                # modify the code and add another elif and handle it by yourself...
+                self.write(f"unhandled control: {char.encode('utf-8')}")
+
+    def thread_read(self):
+        self.__write_input()
+        try:
+            while True:
+                try:
+                    char = readkey()
+                except KeyboardInterrupt:
+                    with self.buffer_lock.for_read():
+                        self.read_interrupted_buffer = self.read_buffer
+                        self.read_interrupted = True
+                    self.__clear_input()
+                    break
+                # self.write(char.encode('utf-8'))
+                if len(char) > 1:
+                    self.handle_stroke(char)
+                    continue
+                delchr = ord(char) == 8 or ord(char) == 127
+                if delchr:
+                    with self.buffer_lock.for_write():
+                        if len(self.read_buffer) != 0 and self.cursor_at > 0:
+                            # delete char before cursor
+                            self.read_buffer = self.read_buffer[:self.cursor_at - 1] + self.read_buffer[self.cursor_at:]
+                            self.cursor_at -= 1
+                            self.__write_input()
+                    continue
+                line_feed = char == '\n' or char == '\r'
+                read_interrupted = ord(char) == 3
+
+                if line_feed or read_interrupted:
+                    self.__clear_input()
+                    if read_interrupted:
+                        with self.buffer_lock.for_read():
+                            self.read_interrupted_buffer = self.read_buffer
+                        self.read_interrupted = True
+                    break
+                if ord(char) <= 31:
+                    continue  # control character
+                with self.write_lock:
+                    with self.buffer_lock.for_write():
+                        self.read_buffer = self.read_buffer[:self.cursor_at] + char + self.read_buffer[self.cursor_at:]
+                        self.cursor_at += 1
+                    self.__write_input()
+        except BaseException:
+            self.read_error = traceback.format_exc()
+            return
+
+    def write(self, txt: object, new_line=True):
+        txt = str(txt)
+        with self.write_lock:
+            if self.read_lock.locked():
+                self.__clear_input()
+                sys.stdout.write(txt)
+                sys.stdout.write('\n')
+                with self.buffer_lock.for_read():
+                    self.__write_input()
+            else:
+                sys.stdout.write(txt)
+                if new_line:
+                    sys.stdout.write('\n')
+                sys.stdout.flush()
+                self.last_line = ''
+            self.last_writer_was_reader = False
+
+    def update_input_buffer(self, txt: str):
+        with self.buffer_lock.for_write():
+            self.read_buffer = txt
+        if self.read_lock.locked():
+            self.__write_input()
+
+    def interrupted_buffer(self):
+        with self.buffer_lock.for_read():
+            return self.read_interrupted_buffer
+
+    
+    def input(self, label: str = None, color=None, history=None):
+        """"
+        Available text colors:
+            grey, red, green, yellow, blue, magenta, cyan, white.        
+        throws KeyboardInterrupt or ReadError
+        """
+        with self.read_lock:
+            self.history = history or []
+            self.history_tail_index = len(self.history)
+            self.read_interrupted_buffer = ''
+            self.read_interrupted = False
+            if label is not None:
+                self.update_input_label(label)
+            self.cursor_at = 0
+            if color is not None:
+                self.update_input_label_color(color)
+            t = threading.Thread(target=self.thread_read)
+            t.daemon = True
+            t.start()
+            t.join()
+            v = self.read_buffer
+            self.read_buffer = ''
+            if self.read_interrupted:
+                self.read_interrupted = False
+                raise KeyboardInterrupt()
+            if self.read_error:
+                err = self.read_error
+                self.read_error = None
+                raise ReadError(err)
+            return v
--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import socket as sockets
 import time
 import traceback
 import hashlib
-
+import _thread
 import mysql.connector
-
+import os
+from cli_io import IO
 
 # interface
 class Resource:
@@ -17,6 +18,10 @@ class Resource:
     def hash(self) -> str: # token
         pass
 
+text_to_commit = ''
+use_resource_next_cycle = False
+running = True
+cli = IO()
 class MySQLResource(Resource):
     def __init__(self, host, database, user, password) -> None:
         super().__init__()
@@ -24,61 +29,76 @@ class MySQLResource(Resource):
         self.database = database
         self.connection = mysql.connector.connect(host=host, database=database, user=user, password=password)
         if self.connection.is_connected():
-            print("Connected to MySQL, Server version: ", self.connection.get_server_info())
+            cli.write("Connected to MySQL, Server version: ", self.connection.get_server_info())
 
-    def use(self, port_col_value):
+    def use(self, data):
+        port_col_value, text = data
         try:
-            print(f"[critical] using mysql database...")
+            cli.write(f"[critical] using mysql database...")
             cursor = self.connection.cursor()
-            cursor.execute("INSERT INTO usage_history (machine_port) VALUES (%s)", (str(port_col_value), ))
+            cursor.execute("INSERT INTO usage_history (machine_port, data) VALUES (%s, %s)", (str(port_col_value), text))
             self.connection.commit()
         except KeyboardInterrupt as e:
             raise e
         except:
-            print(traceback.format_exc())
+            cli.write(traceback.format_exc())
 
     def finalize(self):
         if self.connection.is_connected:
-            print("closing mysql connection")
+            cli.write("closing mysql connection")
             self.connection.close()
 
     def hash(self):
         return hashlib.sha1((self.host+':'+self.database).encode()).hexdigest()
 
+def read_stdin():
+    global use_resource_next_cycle, text_to_commit, running
+    try:
+        while running:
+            if not use_resource_next_cycle:
+                text_to_commit = cli.input("write to db: ", 'magenta')
+                use_resource_next_cycle = True
+            else:
+                time.sleep(.5)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        running = False
 
 def ring_loop(server_socket: sockets.socket, succ_port: int, resource: Resource):
+    global use_resource_next_cycle, text_to_commit, running
     succ_soc = sockets.socket(sockets.AF_INET, sockets.SOCK_STREAM)
-    while True:
-        try:
-            succ_soc.connect(('127.0.0.1', succ_port))
-            break
-        except:
-            pass
-    if 'y' == input("start using?(y/n):"):  # initial wheel push
-        print("sending token to successor")
+    while running:
+        try:succ_soc.connect(('127.0.0.1', succ_port));break
+        except:pass
+    if '1' == os.environ.get('RING_START', ''):  # initial wheel push
+        cli.write("sending token to successor")
         succ_soc.send(resource.hash().encode())
-    pred_soc, _ = server_socket.accept()
+    cli.write('waiting predecessor connection')
+    pred_soc, _ = server_socket.accept() # accept predecessor connection
     our_port = server_socket.getsockname()[1]
     last_iteration_time = time.perf_counter()
     cycle_counter = 0
+    _thread.start_new_thread(read_stdin, ())  # start reading user input
     try:
-        while True:
-            print(f"[cycle {cycle_counter:2d}] waiting token from predecessor")
+        while running:
+            cli.write(f"[cycle {cycle_counter:2d}] waiting token from predecessor")
             token = pred_soc.recv(1024).decode()
             if token == '':
-                print('predecessor link terminated')
+                cli.write('predecessor link terminated')
                 break
-            print(f"[cycle {cycle_counter:2d}] received from predecessor token {token}")
-            if token == resource.hash():
-                print(f"[cycle {cycle_counter:2d}] entering critical section")
-                resource.use(our_port)
-            print(f"[cycle {cycle_counter:2d}] sending to successor {succ_port} token {token}")
+            cli.write(f"[cycle {cycle_counter:2d}] received from predecessor token {token}")
+            if token == resource.hash() and use_resource_next_cycle:
+                cli.write(f"[cycle {cycle_counter:2d}] entering critical section")
+                resource.use([our_port, text_to_commit])
+                use_resource_next_cycle = False
+            cli.write(f"[cycle {cycle_counter:2d}] sending to successor {succ_port} token {token}")
             succ_soc.send(token.encode())
 
             # reduce traffic, 2 seconds per iteration maximum
             delta_t = time.perf_counter() - last_iteration_time
-            # if delta_t < 2:
-            #     time.sleep(2 - delta_t)
+            if delta_t < 2:
+                time.sleep(2 - delta_t)
             last_iteration_time = time.perf_counter()
             cycle_counter += 1
     except KeyboardInterrupt:  # Ctrl+c
@@ -92,7 +112,9 @@ try:
     server_socket.bind(('127.0.0.1', our_port))
     server_socket.listen()
     succ_port = int(input("successor_port="))
+    running = True
     ring_loop(server_socket, succ_port, resource)
-    server_socket.close()
 finally:
+    server_socket.close()
     resource.finalize()
+    running = False
